a182ef044b283e2b25a6e70d6ad7a9f51199e357
Samcraig77 <SAMCRAIG70@GMAIL.COM>
2024-05-31 13:26:03 -0700

    vars for input fields added

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// javascript
+//const messageField = document.getElementById("message-field")
+//const messageFrom = document.getElementById("from-input")
+//const messageTo = document.getElementById("to-input")
+//const publishBtn = document.getElementById("publish-btn");
