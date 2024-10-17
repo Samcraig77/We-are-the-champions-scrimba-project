70df164cbfeceaad9894c5aa996e8ca8f26bed75
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-10-17 16:30:20 -0700

    Like feature and empty form check added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://we-are-the-champs-74996-default-rtdb.firebaseio.com/"
@@ -14,63 +14,81 @@ const messageFrom = document.getElementById("from-input")
 const messageTo = document.getElementById("to-input")
 const publishBtn = document.getElementById("publish-btn")
 const endorsementsArea = document.getElementById("endorsements-area")
-const likeBtn = document.getElementsByClassName("like-btn")
-const likeCount = document.getElementsByClassName("like-count")
 
-
+//Get endorsements from database
 onValue(endorsementsInDB, function(snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
+       
+        endorsementsArea.innerHTML = ""
         
-            
         for (let i = 0; i < itemsArray.length; i++) {
-                let currentItem = itemsArray[i]
-                let messageEl = currentItem[1][0]
-                let fromEl = currentItem[1][1]
-                let toEl = currentItem[1][2]
-                let likesEl = currentItem[1][3]
-                
-                renderEndorsments(messageEl, toEl, fromEl, likesEl)
-                }    
+                let currentPost = itemsArray[i][1]
+                let id = itemsArray[i][0]
+                let messageEl = currentPost["message"]
+                let fromEl = currentPost["from"]
+                let toEl = currentPost["to"]
+                let likeCount = currentPost["likeCount"]
+                renderEndorsements(messageEl, toEl, fromEl, likeCount, id, currentPost)
+            }    
         } else {
-            endorsementsArea.innerHTML = "No endorsments here... yet"
+            endorsementsArea.innerText = "No endorsements here... yet"
         }
-        
 })
 
-function renderEndorsments(message, to, from, likes) {
+//Plugs in the input information that was stored when the form was submitted and displays it
+function renderEndorsements(message, to, from, likeCount, id) {
+
         endorsementsArea.innerHTML += `
-        <div class="message"><p> 
-            <p>
-                Hey, 
-                <span class="to-user">${to}
+        <div class="message">
+            <p> Hey, 
+                <span class="to-user">${to}</span>
             </p> 
             <p>${message}</p> 
-            <span>From: ${from} 
-                <span class="likes-container">
-                    <span class="like-count">${likes}</span>
-                    <button class="like-btn">😸</button>
-                </span>
-            </span>
-        
+            <div class="bottom-msg">
+            <p>From: <span class="from-user">${from}</span></p>
+            <button class="like-count like-btn" data-id=${id} data-likes=${likeCount}>${likeCount} ❤️</button>
+            </div>
         </div>
         `
 }
 
-//likeBtn.addEventListener("click", function())
-
-
-// function addLike(like) {
-    
-// }
-
-publishBtn.addEventListener("click", function() {
-   if (messageField.value != null) {
-    let message = messageField.value
-    let from = messageFrom.value
-    let to = messageTo.value
-    let likeCount = 0
-    push(endorsementsInDB, [message, from, to, likeCount])
-   }
+endorsementsArea.addEventListener("click", function(event){
+    console.log(event.target)
+    if (event.target.dataset) {
+        handleLikes(event.target.dataset.id, event.target.dataset.likes)
+    }
 })
 
+
+function handleLikes(id, likeCount) {
+        // Specify the path to the endorsement
+        const endorsementRef = ref(database, `endorsements/${id}`)
+
+        // New likes value
+        let newLikeCount = Number(likeCount) + 1 // we use Number() to turn a string (the dataset is a string type) into a number
+
+        // Update the likes value
+        update(endorsementRef, {
+            likeCount: newLikeCount
+        })
+}
+
+//Takes the form values and stores them
+publishBtn.addEventListener("click", function(event) {
+    event.preventDefault()
+    if (messageField.value && messageFrom.value && messageTo.value != null) {
+        const postData = {
+        message: messageField.value,
+        from: messageFrom.value,
+        to: messageTo.value,
+        likeCount: 0 
+        }
+    push(endorsementsInDB, postData)
+    messageField.value = ""
+    messageFrom.value = ""
+    messageTo.value = ""
+    } else {
+        console.error("Please fill out the entire form");
+    }
+})
